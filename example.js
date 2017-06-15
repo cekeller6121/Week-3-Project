d04672064d914c8b6953aeafe8e7df12489f0bd9
chadwick <cekeller6121@yahoo.com>
2017-06-15 13:36:07 -0400

    project complete

---
+++ example.js
@@ -0,0 +1,1 @@
+var nums = document.getElementsByClassName('button'):
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // *** Chad's calculator:
 
 // This is my first JavaScript project at The Iron Yard
@@ -14,11 +13,24 @@ var values2 = [];
 // is pressed at the end of the equation, it knows what to do.
 var flag;
 var flagAdd;
-var flagMinus;
+var flagsubtract;
 var flagMultiply;
 var flagDivide;
+var six_counter = 0;
 
-var button = document.querySelectorAll('.button');
+// This function resets all flags so that we can continue to do operations
+// after the equals operation is completed.
+
+function operatorFlagsFalse() {
+  flag = false;
+  flagAdd = false;
+  flagsubtract = false;
+  flagMultiply = false;
+  flagDivide = false;
+};
+
+// *** Number button assignments ***
+var button = document.getElementsByClassName('button');
 var zero_button = document.getElementById('zero-button');
 var one_button = document.getElementById('one-button');
 var two_button = document.getElementById('two-button');
@@ -32,7 +44,7 @@ var nine_button = document.getElementById('nine-button');
 
 // *** Operator button assignments ***
 var add_button = document.getElementById('add-button');
-var minus_button = document.getElementById('minus-button');
+var subtract_button = document.getElementById('subtract-button');
 var multiply_button = document.getElementById('multiply-button');
 var divide_button = document.getElementById('divide-button');
 var equals_button = document.getElementById('equals-button');
@@ -59,6 +71,7 @@ one_button.addEventListener('click', function oneClick() {
   //      that number is a part of a number before we choose an operator
   //      and needs to be stored in the first array which is values1.
   let one_button = 1;
+  document.getElementById('output').innerHTML = one_button;
     if (flag === true) {
       values2.push(one_button);
     } else {
@@ -69,6 +82,7 @@ one_button.addEventListener('click', function oneClick() {
 two_button.addEventListener('click', function twoClick() {
   console.log("two button click");
   let two_button = 2;
+  document.getElementById('output').innerHTML = two_button;
   if (flag === true) {
     values2.push(two_button);
   } else {
@@ -79,6 +93,7 @@ two_button.addEventListener('click', function twoClick() {
 three_button.addEventListener('click', function threeClick () {
   console.log("three button click");
   let three_button = 3;
+  document.getElementById('output').innerHTML = three_button;
   if (flag === true) {
     values2.push(three_button);
   } else {
@@ -89,6 +104,7 @@ three_button.addEventListener('click', function threeClick () {
 four_button.addEventListener('click', function fourClick () {
   console.log("four button click");
   let four_button = 4;
+  document.getElementById('output').innerHTML = four_button;
   if (flag === true) {
     values2.push(four_button);
   } else {
@@ -99,6 +115,7 @@ four_button.addEventListener('click', function fourClick () {
 five_button.addEventListener('click', function fiveClick () {
   console.log("five button click");
   let five_button = 5;
+  document.getElementById('output').innerHTML = five_button;
   if (flag === true) {
     values2.push(five_button);
   } else {
@@ -109,16 +126,22 @@ five_button.addEventListener('click', function fiveClick () {
 six_button.addEventListener('click', function sixClick () {
   console.log("six button click");
   let six_button = 6;
+  six_counter += 1;
+  document.getElementById('output').innerHTML = six_button;
   if (flag === true) {
     values2.push(six_button);
   } else {
     values1.push(six_button);
+  }
+  if (six_counter === 3) {
+    document.getElementById('output').innerHTML = "ruh roh!";
   }
 });
 
 seven_button.addEventListener('click', function sevenClick () {
   console.log("seven button click");
   let seven_button = 7;
+  document.getElementById('output').innerHTML = seven_button;
   if (flag === true) {
     values2.push(seven_button);
   } else {
@@ -129,6 +152,7 @@ seven_button.addEventListener('click', function sevenClick () {
 eight_button.addEventListener('click', function eightClick() {
   console.log("eight button click");
   let eight_button = 8;
+  document.getElementById('output').innerHTML = eight_button;
   if (flag === true) {
     values2.push(eight_button);
   } else {
@@ -139,12 +163,13 @@ eight_button.addEventListener('click', function eightClick() {
 nine_button.addEventListener('click', function nineClick() {
   console.log("nine button click");
   let nine_button = 9;
+  document.getElementById('output').innerHTML = nine_button;
   if (flag === true) {
     values2.push(nine_button);
   } else {
     values1.push(nine_button);
   }
-})
+});
 
 // *** Operator button event listeners ***
 
@@ -158,23 +183,37 @@ equals_button.addEventListener('click', function equalsClick() {
   values2 = values2.join("");
   values2 = Number(values2);
 
+  function resultTooBig() {
+    if (result.length > 10) {
+      alert(result);
+      document.getElementById('output').innerHTML = "Big number, whooo!";
+    }
+  };
+
   if (flagAdd === true) {
     result = values1 + values2;
     console.log(result);
     document.getElementById('output').innerHTML = result;
-  } else if (flagMinus === true) {
+  } else if (flagsubtract === true) {
     result = values1 - values2;
     console.log(result);
     document.getElementById('output').innerHTML = result;
   } else if (flagMultiply === true) {
     result = values1 * values2;
     console.log(result);
+    resultTooBig();
     document.getElementById('output').innerHTML = result;
   } else if (flagDivide === true) {
     result = values1 / values2;
     console.log(result);
+    resultTooBig();
     document.getElementById('output').innerHTML = result;
   }
+    operatorFlagsFalse();
+    values1 = [];
+    values2 = [];
+    values1.push("" + result);
+    result = 0;
 });
 
 add_button.addEventListener('click', function addOperator() {
@@ -183,14 +222,16 @@ add_button.addEventListener('click', function addOperator() {
   flagAdd = true;
   values1 = values1.join("");
   values1 = Number(values1);
+  document.getElementById('output').innerHTML = values1;
 });
 
-minus_button.addEventListener('click', function minusOperator () {
-  console.log("minus button click");
+subtract_button.addEventListener('click', function subtractOperator () {
+  console.log("subtract button click");
   flag = true;
-  flagMinus = true;
+  flagsubtract = true;
   values1 = values1.join("");
   values1 = Number(values1);
+  document.getElementById('output').innerHTML = values1;
 });
 
 multiply_button.addEventListener('click', function multiplyOperator () {
@@ -199,6 +240,7 @@ multiply_button.addEventListener('click', function multiplyOperator () {
   flagMultiply = true;
   values1 = values1.join("");
   values1 = Number(values1);
+  document.getElementById('output').innerHTML = values1;
 });
 
 divide_button.addEventListener('click', function divideOperator() {
@@ -207,6 +249,7 @@ divide_button.addEventListener('click', function divideOperator() {
   flagDivide = true;
   values1 = values1.join("");
   values1 = Number(values1);
+  document.getElementById('output').innerHTML = values1;
 });
 
 
