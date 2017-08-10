e1e24e66b44a8c704901c07e72bc831c54ffeba9
chadwick <cekeller6121@yahoo.com>
2017-08-10 12:33:26 -0400

    add bootstrap styling, push input values to output display

--- revised.js
+++ revised.js
@@ -2,10 +2,11 @@ console.log("revised.js file connected");
 
 // *** this is a new js file- I'm going to do my best to make this app a little bit lighter than I did on week 3 of The Iron Yard ***
 
-var calcButtons = document.querySelectorAll(".button");
+var calcButtons = document.querySelectorAll(".btn-success");
 for (i = 0; i < calcButtons.length; i++) {
         calcButtons[i].addEventListener("click", function(event) {
               console.log(event.target.value);
+              document.getElementById('output').innerHTML = event.target.value;
         });
 };
 
